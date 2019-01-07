7094ab35d6fcd73d0bf63789133c023558f03e22
martinek <martinek@UCEBNA-NB-01.gjkt.local>
2019-01-07 15:54:11 +0100

    Update 7 - application fully functional

--- Projekt - Kalkulačka/src/application/Main.java
+++ Projekt - Kalkulačka/src/application/Main.java
@@ -1,42 +1,22 @@
 package application;
 	
-import java.io.IOException;
-
-import controller.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	
-	private BorderPane rootLayout;
-	private Controller Controller;
-	
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			initRootLayout(primaryStage);
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void initRootLayout(Stage stage) throws IOException {
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(Main.class.getResource("/view/RootLayout.fxml"));
-		rootLayout = (BorderPane) loader.load();
-		Controller = loader.getController();
-		Scene scene = new Scene(rootLayout,800,600);
-		stage.setScene(scene);
-		stage.show();
-	}
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/RootLayout.fxml"));
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- Projekt - Kalkulačka/src/controller/Controller.java
+++ Projekt - Kalkulačka/src/controller/Controller.java
@@ -6,33 +6,57 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import model.Model;
 
 public class Controller {
 
 	@FXML
-    private TextField texto;
+    private Text texto;
+	
+	private long cislo1 = 0;
+	private String operator = "";
+	private boolean start = true;
+
+    private Model model = new Model();
 	
 	public Controller(){
 		
 	}
 	
 	@FXML
-	private void initialize(){
+    private void processNumpad(ActionEvent event) {
+        if(start) {
+        	texto.setText("");
+        	start = false;
+        }
+		
+		String value = ((Button)event.getSource()).getText();
+        texto.setText(texto.getText() + value);
 	}
+    
+	 @FXML
+	    private void processOperator(ActionEvent event) {
+	        String value = ((Button)event.getSource()).getText();
 
-	
-	
-	@FXML
-    private void handleButtonAction(ActionEvent event)
-    {
-        Button button = (Button)event.getSource();
-        texto.appendText(button.getText());
-	
-        
-      
-}
-}
+	        if (!"=".equals(value)) {
+	            if (!operator.isEmpty())
+	                return;
+
+	            operator = value;
+	            cislo1 = Long.parseLong(texto.getText());
+	            texto.setText("");
+	        }
+	        else {
+	            if (operator.isEmpty())
+	                return;
+
+	            texto.setText(String.valueOf(model.calculate(cislo1, Long.parseLong(texto.getText()), operator)));
+	            operator = "";
+	            start = true;
+	        }
+	    }
+	}
 
 
 
